b16ddf177765ae002f2f353b426bf2c71fc9522d
Joshua Vial <joshua@enspiral.com>
2013-07-26 03:07:00 +1200

    fixing karma gem install

--- lib/rails_karma/tasks.rb
+++ lib/rails_karma/tasks.rb
@@ -6,22 +6,21 @@ namespace :karma do
 
   desc 'Install karma files'
   task :install do
-    #sh 'echo "{}" > package.json' unless File.exists?('package.json')
-    #sh 'npm install karma --save-dev'
+    sh 'echo "{}" > package.json' unless File.exists?('package.json')
+    sh 'npm install karma --save-dev'
     unless File.exists?('karma')
       sh 'mkdir karma'
-      sh 'mkdir karma/angular'
+      sh 'mkdir karma/lib'
+      sh 'mkdir karma/lib/angular'
+
+      mocks_source = File.join(Gem.loaded_specs["rails_karma"].full_gem_path, 'lib', 'rails_karma', 'files', 'angular-mocks.js')
+      mocks_target = File.join(Rails.root, 'karma', 'lib', 'angular', 'angular-mocks.js')
+      FileUtils.cp_r mocks_source, mocks_target
     end
 
-    #copy files
-    config_source = File.join(Gem.loaded_specs["rails_karma"].full_gem_path, 'files', 'karma.conf.js')
+    config_source = File.join(Gem.loaded_specs["rails_karma"].full_gem_path, 'lib', 'rails_karma', 'files', 'karma.conf.js')
     config_target = File.join(Rails.root, 'config', 'karma.conf.js')
-    FileUtils.cp_r config_source, config_target
-
-    mocks_source = File.join(Gem.loaded_specs["rails_karma"].full_gem_path, 'files', 'angular-mocks.js')
-    mocks_target = File.join(Rails.root, 'karma', 'angular', 'angular-mocks.js')
-    FileUtils.cp_r mocks_source, mocks_target
-
+    FileUtils.cp_r config_source, config_target unless File.exists?(config_target)
   end
 end
 
